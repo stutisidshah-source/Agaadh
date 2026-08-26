fc6fde3aaf347f65b4e78a32735eb6f77bf077d2
Stuti Shah <stutisidshah@gmail.com>
2026-08-26 11:55:24 +0530

    test: exclude e2e directory from vitest runs

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: ['./src/setupTests.ts'],
     globals: true,
+    exclude: ['node_modules', 'dist', 'e2e/**'],
   },
   resolve: {
     alias: {
